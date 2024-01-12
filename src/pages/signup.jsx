820bfbca3de16f0ede8d406926b26db5205d0244
m-maaz-azhar <maazazher28@gmail.com>
2024-01-12 15:34:21 +0500

    fixed a minor bug

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import Header from "../components/header";
+import Header from "../components/Header";
 import { withSessionSsr } from "./api/lib/withSession";
 
 export default function SignUp() {
