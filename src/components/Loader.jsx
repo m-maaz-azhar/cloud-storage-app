6b343bd2546b578d4a57d68739582313eaf6d8ec
m-maaz-azhar <maazazher28@gmail.com>
2024-01-12 15:32:20 +0500

    fixed a minor bug

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Loader() {
   return (
     <div className="flex items-center justify-center h-[70vh] ">
-      <span class="loader"></span>
+      <span className="loader"></span>
     </div>
   );
 }
